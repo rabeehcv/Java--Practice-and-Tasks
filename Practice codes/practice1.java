ea64522f03475dd0eca76fa7af2d19f5e7ffa892
Rabeeh cv <125405136+rabeehcv@users.noreply.github.com>
2024-07-17 12:36:00 +0530

    Update practice1.java

--- Practice codes/practice1.java
+++ Practice codes/practice1.java
@@ -1,18 +1,23 @@
 class Dog{
+    //Fields(states)
     String name;
     String breed;
     int age;
 
+    //Methods(Behavior)
     void bark(){
         System.out.println(age + " years old "+ name + " is barking and the breed of this dog is " + breed);
     }
 }
 public class practice {
     public static void main(String[] args) {
+        //Creating an object
         Dog myDog = new Dog();
         myDog.name = "Bruno";
         myDog.breed = "German shepherd";
         myDog.age = 10;
+
+        //Using an object
         myDog.bark();
     }
 }
